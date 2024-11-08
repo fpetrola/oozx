c8baab7e2650d828ed222c871b2e2a594de3291c
Fernando Petrola <fernandop@sonatype.com>
2024-11-08 00:06:29 -0300

    blocks and routines modules

--- routines/src/main/java/com/fpetrola/z80/transformations/VirtualAssignmentInstruction.java
+++ routines/src/main/java/com/fpetrola/z80/transformations/VirtualAssignmentInstruction.java
@@ -18,6 +18,7 @@
 
 package com.fpetrola.z80.transformations;
 
+import com.fpetrola.z80.instructions.base.DummyInstruction;
 import com.fpetrola.z80.opcodes.references.WordNumber;
 import com.fpetrola.z80.registers.Register;
 
