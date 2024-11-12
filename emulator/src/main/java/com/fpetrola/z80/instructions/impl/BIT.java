c6bfb0122928721ed648270332a93c78c7a2a119
Fernando Petrola <fernandop@sonatype.com>
2024-11-12 18:14:11 -0300

    using TableAluOperation

--- emulator/src/main/java/com/fpetrola/z80/instructions/impl/BIT.java
+++ emulator/src/main/java/com/fpetrola/z80/instructions/impl/BIT.java
@@ -20,6 +20,7 @@ package com.fpetrola.z80.instructions.impl;
 
 import com.fpetrola.z80.instructions.types.BitOperation;
 import com.fpetrola.z80.base.InstructionVisitor;
+import com.fpetrola.z80.opcodes.references.MemoryPlusRegister8BitReference;
 import com.fpetrola.z80.opcodes.references.OpcodeReference;
 import com.fpetrola.z80.opcodes.references.WordNumber;
 import com.fpetrola.z80.registers.Register;
@@ -27,10 +28,15 @@ import com.fpetrola.z80.registers.flag.AluOperation;
 
 public class BIT<T extends WordNumber> extends BitOperation<T> {
   public static final AluOperation testBitTableAluOperation = new AluOperation() {
-    public int execute(int bit, int value, int carry) {
-      int s = data & SIGN_MASK;
+    @Override
+    public <T extends WordNumber> T executeWithCarry2(T value_, T bit_, int carry, Register<T> flag) {
+      data = flag.read().intValue();
       int f = data;
+      int value = value_.intValue();
+
+      int s = data & SIGN_MASK;
       f = (f & CARRY_MASK) | HALFCARRY_MASK | (value & (FLAG_3 | FLAG_5));
+      int bit = bit_.intValue();
       int mask = 1 << bit;
       boolean zeroFlag = (mask & value) == 0;
 
@@ -41,8 +47,10 @@ public class BIT<T extends WordNumber> extends BitOperation<T> {
         f |= SIGN_MASK;
       }
       data = f;
-      return value;
+      SetFlags53From(carry);
+      return WordNumber.createValue(data);
     }
+
   };
 
   public BIT(OpcodeReference target, int n, Register<T> flag) {
@@ -51,7 +59,11 @@ public class BIT<T extends WordNumber> extends BitOperation<T> {
 
   public int execute() {
     final T value = target.read();
-    testBitTableAluOperation.executeWithCarry(value, WordNumber.createValue(n), flag);
+    int address = value.intValue();
+    if (target instanceof MemoryPlusRegister8BitReference<T> memoryPlusRegister8BitReference) {
+      address = memoryPlusRegister8BitReference.getTarget().read().plus(memoryPlusRegister8BitReference.fetchRelative()).intValue() >> 8;
+    }
+    flag.write(testBitTableAluOperation.executeWithCarry2(value, WordNumber.createValue(n), address, flag));
     return cyclesCost;
   }
 
