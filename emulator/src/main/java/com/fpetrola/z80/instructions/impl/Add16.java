aec4009d71abdad59ba870d4cd9c5bb535ec0041
Fernando Petrola <fernandop@sonatype.com>
2024-11-21 12:20:25 -0300

    add16 with table

--- emulator/src/main/java/com/fpetrola/z80/instructions/impl/Add16.java
+++ emulator/src/main/java/com/fpetrola/z80/instructions/impl/Add16.java
@@ -25,11 +25,19 @@ import com.fpetrola.z80.opcodes.references.OpcodeReference;
 import com.fpetrola.z80.opcodes.references.WordNumber;
 import com.fpetrola.z80.registers.Register;
 import com.fpetrola.z80.registers.flag.AluOperation;
+import com.fpetrola.z80.registers.flag.TableAluOperation;
+
+import static com.fpetrola.z80.opcodes.references.WordNumber.createValue;
 
 public class Add16<T extends WordNumber> extends ParameterizedBinaryAluInstruction<T> {
-  public static final AluOperation add16TableAluOperation = new AluOperation() {
-    public int execute(int value1, int value2, int carry) {
-      int add16temp = value1 + value2;
+  public static final AluOperation add16TableAluOperation = new TableAluOperation() {
+    public int execute(int add16temp1, int value1, int value2) {
+      F = add16temp1;
+      getValue1((value1 & 1) << 11, (value1 & 2) << 10, value1 << 9);
+      return F;
+    }
+
+    private int getValue1(int value1, int value2, int add16temp) {
       int lookup = ((value1 & 0x0800) >> 11) |
           ((value2 & 0x0800) >> 10) |
           ((add16temp & 0x0800) >> 9);
@@ -44,7 +52,15 @@ public class Add16<T extends WordNumber> extends ParameterizedBinaryAluInstructi
   };
 
   public Add16(OpcodeReference target, ImmutableOpcodeReference source, Register<T> flag) {
-    super(target, source, flag, (tFlagRegister, value2, value) -> add16TableAluOperation.executeWithCarry2(value2, value, tFlagRegister.read().intValue(), tFlagRegister));
+    super(target, source, flag, (tFlagRegister, v2, v1) -> {
+      int value1 = v1.intValue();
+      int value2 = v2.intValue();
+      int add16temp = value1 + value2;
+      int i = value1 >> 11 & 0x01 | value2 >> 10 & 0x02 | ((add16temp & 0x13800) >> 9);
+      T t = add16TableAluOperation.executeWithCarry2(createValue(i), createValue(flag.read().intValue()), 1, tFlagRegister);
+      flag.write(t);
+      return createValue(add16temp);
+    });
   }
 
   @Override
