015890080b68ffd933bec5acd7d7924ee5ba2fd7
Fernando Petrola <fernandop@sonatype.com>
2024-11-29 15:45:25 -0300

    fix

--- routines/src/main/java/com/fpetrola/z80/se/SymbolicExecutionAdapter.java
+++ routines/src/main/java/com/fpetrola/z80/se/SymbolicExecutionAdapter.java
@@ -233,7 +233,7 @@ public class SymbolicExecutionAdapter<T extends WordNumber> {
         ready |= stackFrames.isEmpty();
         lastPc = pcValue;
 
-        System.out.println("PC: " + Helper.formatAddress(pcValue));
+//        System.out.println("PC: " + Helper.formatAddress(pcValue));
 //        System.out.println("BC: " + Helper.formatAddress(state.getRegister(RegisterName.BC).read().intValue()));
 
       }
