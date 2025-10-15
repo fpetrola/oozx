3f06764c35a364c245049406dee91162745b38dc
Fernando Petrola <fernandop@sonatype.com>
2025-10-15 15:25:05 -0300

    DefaultInstructionFetcher.java

--- emulator/src/main/java/com/fpetrola/z80/cpu/DefaultInstructionFetcher.java
+++ emulator/src/main/java/com/fpetrola/z80/cpu/DefaultInstructionFetcher.java
@@ -123,7 +123,6 @@ public class DefaultInstructionFetcher<T extends WordNumber> implements Instruct
 
       state.getPc().write(nextPC);
     } catch (Exception e) {
-      e.printStackTrace();
       state.setRunState(State.RunState.STATE_STOPPED_BREAK);
     }
   }
