752520a3f1e1a2397a3dba2858c265b9b66c41c6
Fernando Petrola <fernandop@sonatype.com>
2025-11-03 19:49:24 -0300

    moving memptr

--- emulator/src/main/java/com/fpetrola/z80/instructions/impl/And.java
+++ emulator/src/main/java/com/fpetrola/z80/instructions/impl/And.java
@@ -22,11 +22,10 @@ import com.fpetrola.z80.base.InstructionVisitor;
 import com.fpetrola.z80.instructions.types.ParameterizedBinaryAluInstruction;
 import com.fpetrola.z80.opcodes.references.ImmutableOpcodeReference;
 import com.fpetrola.z80.opcodes.references.OpcodeReference;
-import com.fpetrola.z80.opcodes.references.WordNumber;
 import com.fpetrola.z80.registers.Register;
 import com.fpetrola.z80.registers.flag.TableAluOperation;
 
-public class And<T extends WordNumber> extends ParameterizedBinaryAluInstruction<T> {
+public class And<T> extends ParameterizedBinaryAluInstruction<T> {
   protected static final TableAluOperation andTableAluOperation = new TableAluOperation() {
     public int execute(int A, int value, int carry) {
       A &= value;
