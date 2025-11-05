94cce33b48ee0e62993b253f176429998b38347f
fernando <fpetrola@dc.uba.ar>
2025-11-05 20:44:51 -0300

    The pager a 128 and a +3 have on board
    Writing 0x7ffd moves a bank to 0xc000 and picks the ROM; on a +3, 0x1ffd puts
    RAM where the ROM was. It is a device on the bus and not a capability the
    machine declares.

---
+++ machine/machines/memory/src/main/java/com/fpetrola/oozx/speccy/devices/memory/SpecPlus3PortHandler.java
@@ -0,0 +1,37 @@
+/*
+ *
+ *  * Copyright (c) 2023-2025 Fernando Damian Petrola
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.fpetrola.oozx.speccy.devices.memory;
+
+import com.fpetrola.oozx.speccy.ports.DefaultPortHandler;
+
+import java.util.function.Supplier;
+import com.fpetrola.oozx.speccy.machine.PagingPlus3;
+
+class SpecPlus3PortHandler extends DefaultPortHandler {
+  private final Supplier<PagingPlus3> machine;
+
+  public SpecPlus3PortHandler(Supplier<PagingPlus3> machine) {
+    super(false, true);
+    this.machine = machine;
+  }
+
+  public void write(int port, byte value) {
+    machine.get().memoryPort2Write(port, value);
+  }
+}
