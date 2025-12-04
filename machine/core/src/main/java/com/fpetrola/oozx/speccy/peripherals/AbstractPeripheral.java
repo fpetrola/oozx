3f4d30f64fd9882aa16f887f5eae30d3652399cb
fernando <fpetrola@dc.uba.ar>
2025-12-03 21:31:28 -0300

    The machines themselves, each one a Spectrum with different numbers
    A 48K, a 128, a +2, a +3, a Pentagon and the rest are not different emulators:
    the same machine with different timings, a different memory map and different
    chips on board. Each is a class that says its numbers, and what it comes with
    plugged in stays with it.

---
+++ machine/core/src/main/java/com/fpetrola/oozx/speccy/peripherals/AbstractPeripheral.java
@@ -0,0 +1,64 @@
+/*
+ *
+ *  * Copyright (c) 2023-2025 Fernando Damian Petrola
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.fpetrola.oozx.speccy.peripherals;
+
+import com.fpetrola.oozx.speccy.machine.SpectrumMachine;
+import com.fpetrola.oozx.speccy.ports.Wired;
+
+import java.util.List;
+
+public class AbstractPeripheral implements Peripheral {
+  private Wired[] portHandlers;
+
+  public AbstractPeripheral(List<Wired> portHandlers) {
+    this.portHandlers = portHandlers.toArray(new Wired[0]);
+  }
+
+  public void activate(SpectrumMachine machine) {
+  }
+
+  public void deactivate() {
+  }
+
+  /**
+   * For a peripheral whose ports talk back to it: they cannot be handed to super, because
+   * there is no this to give them yet.
+   */
+  protected void ports(Wired... handlers) {
+    this.portHandlers = handlers;
+  }
+
+  public Wired[] getPorts() {
+    return portHandlers;
+  }
+
+  /** Built in: on the board of a machine that names it. One that is plugged in says where it fits. */
+  public boolean fitsOn(SpectrumMachine machine) {
+    return machine.onBoard().contains(getClass());
+  }
+
+  /** Built in: nobody is asked. One that is plugged in by choice answers from what was chosen. */
+  public boolean isWanted() {
+    return true;
+  }
+
+  public boolean hasHardReset() {
+    return false;
+  }
+}
---
+++ machine/core/src/main/java/com/fpetrola/oozx/speccy/peripherals/PluggablePeripheral.java
@@ -0,0 +1,47 @@
+/*
+ *
+ *  * Copyright (c) 2023-2025 Fernando Damian Petrola
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.fpetrola.oozx.speccy.peripherals;
+
+import com.fpetrola.oozx.speccy.ports.Wired;
+
+import java.util.List;
+
+/** The usual way of being {@link Pluggable}: a flag, which is what {@link #isWanted} answers. */
+public abstract class PluggablePeripheral extends AbstractPeripheral implements Pluggable {
+  private boolean pluggedIn;
+
+  protected PluggablePeripheral(List<Wired> ports) {
+    super(ports);
+  }
+
+  @Override
+  public void plugIn(boolean connected) {
+    pluggedIn = connected;
+  }
+
+  @Override
+  public boolean isPluggedIn() {
+    return pluggedIn;
+  }
+
+  @Override
+  public boolean isWanted() {
+    return pluggedIn;
+  }
+}
