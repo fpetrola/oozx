8981dd46524fbf92cac4167d4d01c5162f08e763
Fernando Petrola <fernandop@sonatype.com>
2025-12-11 15:33:29 -0300

    bytecode inliner

--- bytecode/src/test/java/com/fpetrola/oozx/Z80UnRolled.java
+++ bytecode/src/test/java/com/fpetrola/oozx/Z80UnRolled.java
@@ -7,14 +7,47 @@ import com.fpetrola.z80.registers.Register;
 public class Z80UnRolled {
   // ============ ALU Operations ============
   // All ALU operations initialized from their respective instruction classes
-  public Add.AddTableAluOperation addTableAluOperation;
-  public Adc.AdcTableAluOperation adcTableAluOperation;
-  public Sub.SubTableAluOperation subTableAluOperation;
-  public Sbc.SbcTableAluOperation sbcTableAluOperation;
-  public And.AndTableAluOperation andTableAluOperation;
-  public Xor.XorTableAluOperation xorTableAluOperation;
-  public Or.OrTableAluOperation orTableAluOperation;
-  public Cp.CpTableAluOperation cpTableAluOperation;
+  RLA.RLAAluOperation rLAAluOperation = new RLA.RLAAluOperation();
+  RRA.RRAAluOperation rRAAluOperation = new RRA.RRAAluOperation();
+  RLCA.RlcaTableAluOperation rlcaTableAluOperation = new RLCA.RlcaTableAluOperation();
+  RRCA.RRCAAluOperation rRCAAluOperation = new RRCA.RRCAAluOperation();
+  RLC.RlcTable1AluOperation rlcTable1AluOperation = new RLC.RlcTable1AluOperation();
+  RRC.RRCAluOperation rRCAluOperation = new RRC.RRCAluOperation();
+  RL.RlTableAluOperation rlTableAluOperation = new RL.RlTableAluOperation();
+  RR.RrTableAluOperation rrTableAluOperation = new RR.RrTableAluOperation();
+  SLA.SlaTableAluOperation slaTableAluOperation = new SLA.SlaTableAluOperation();
+  SRA.SRAAluOperation sRAAluOperation = new SRA.SRAAluOperation();
+  SRL.SrlTableAluOperation srlTableAluOperation = new SRL.SrlTableAluOperation();
+  SLL.SLLAluOperation sLLAluOperation = new SLL.SLLAluOperation();
+  RLD.RldTableAluOperation rldTableAluOperation = new RLD.RldTableAluOperation();
+  RRD.RrdTableAluOperation rrdTableAluOperation = new RRD.RrdTableAluOperation();
+  Add.AddTableAluOperation addTableAluOperation = new Add.AddTableAluOperation();
+  Adc.AdcTableAluOperation adcTableAluOperation = new Adc.AdcTableAluOperation();
+  Sub.SubTableAluOperation subTableAluOperation = new Sub.SubTableAluOperation();
+  Sbc.SbcTableAluOperation sbcTableAluOperation = new Sbc.SbcTableAluOperation();
+  And.AndTableAluOperation andTableAluOperation = new And.AndTableAluOperation();
+  Xor.XorTableAluOperation xorTableAluOperation = new Xor.XorTableAluOperation();
+  Or.OrTableAluOperation orTableAluOperation = new Or.OrTableAluOperation();
+  Cp.CpTableAluOperation cpTableAluOperation = new Cp.CpTableAluOperation();
+  Inc.Inc8TableAluOperation inc8TableAluOperation = new Inc.Inc8TableAluOperation();
+  Dec.Dec8TableAluOperation dec8TableAluOperation = new Dec.Dec8TableAluOperation();
+  DAA.DaaTableAluOperation daaTableAluOperation = new DAA.DaaTableAluOperation();
+  CPL.CplTableAluOperation cplTableAluOperation = new CPL.CplTableAluOperation();
+  SCF.ScfTableAluOperation scfTableAluOperation = new SCF.ScfTableAluOperation();
+  CCF.CcfTableAluOperation ccfTableAluOperation = new CCF.CcfTableAluOperation();
+  BIT.BitAluOperation bitAluOperation = new BIT.BitAluOperation();
+  Ini.IniTableAluOperation iniTableAluOperation = new Ini.IniTableAluOperation();
+  Ldi.LdiTableAluOperation ldiTableAluOperation = new Ldi.LdiTableAluOperation();
+  Cpi.CpiTableAluOperation cpiTableAluOperation = new Cpi.CpiTableAluOperation();
+  LdAI.LdaiTableAluOperation ldaiTableAluOperation = new LdAI.LdaiTableAluOperation();
+  LdAR.LdarTableAluOperation ldarTableAluOperation = new LdAR.LdarTableAluOperation();
+  Add16.Add16TableAluOperation add16TableAluOperation = new Add16.Add16TableAluOperation();
+  Adc16.Adc16TableAluOperation adc16TableAluOperation = new Adc16.Adc16TableAluOperation();
+  Sbc16.Sbc16TableAluOperation sbc16TableAluOperation = new Sbc16.Sbc16TableAluOperation();
+  Cpd.CpdTableAluOperation cpdTableAluOperation = new Cpd.CpdTableAluOperation();
+  In.InAluOperation inAluOperation = new In.InAluOperation();
+  Outi.OutiTableAluOperation outiTableAluOperation = new Outi.OutiTableAluOperation();
+  Neg.NegTableAluOperation negTableAluOperation = new Neg.NegTableAluOperation();
 
   // ============ 8-bit Registers (Primary Set) ============
   private int A;      // Accumulator
