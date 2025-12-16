25cdfd1d7ceba5b1f3ba72c29c590b0e31b4828f
Fernando Petrola <fernandop@sonatype.com>
2025-12-16 15:17:28 -0300

    inliner bytecode step 1

--- bytecode/src/test/java/com/fpetrola/oozx/t2/InstanceInlinerWithSootUpTest.java
+++ bytecode/src/test/java/com/fpetrola/oozx/t2/InstanceInlinerWithSootUpTest.java
@@ -1,19 +1,23 @@
 package com.fpetrola.oozx.t2;
 
 import com.fpetrola.oozx.MyAbstractMemory;
+import com.fpetrola.z80.registers.RegisterBank;
+import com.fpetrola.z80.registers.RegisterName;
+import com.fpetrola.z80.registers.UnrolledRegisterBank;
+import com.fpetrola.z80.registers.UnrolledRegisterBankFactory;
 import org.junit.Test;
 
 import java.io.IOException;
 
+import static com.fpetrola.z80.registers.RegisterName.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InstanceInlinerWithSootUpTest {
 
   @Test
   public void testClassClonerGeneratesValidBytecode() throws IOException {
-    var target = new MemoryPlusRegister8BitReference(
-        new Plain16BitRegister("IX"), new MyAbstractMemory(), new Plain16BitRegister("PC"), 2
-    );
+    RegisterBank bank = new UnrolledRegisterBankFactory().createBank();
+    var target = new MemoryPlusRegister8BitReference(bank.get(IX), new MyAbstractMemory(), bank.get(PC), 2);
 
     String decompiledSource = inlineAndDecompile(target);
 
