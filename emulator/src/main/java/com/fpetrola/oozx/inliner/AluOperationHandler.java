8f786489ebb023cfbfee85466f4ca35b1aa4f5ce
Fernando Petrola <fernandop@sonatype.com>
2025-12-21 00:09:37 -0300

    dd step 1

--- emulator/src/main/java/com/fpetrola/oozx/inliner/AluOperationHandler.java
+++ emulator/src/main/java/com/fpetrola/oozx/inliner/AluOperationHandler.java
@@ -165,9 +165,9 @@ public class AluOperationHandler {
 
     aluOp.field(FLAG).set(flagField);
 
-    // Ejecutar: result = aluOperation.execute2ValuesAndCarry(value, 0, flag)
+    // Ejecutar: result = aluOperation.execute2ValuesAndCarry(value, flag, flag)
     Variable result = mm.var(int.class);
-    result.set(aluOp.invoke("execute2ValuesAndCarry", value, 0, flag));
+    result.set(aluOp.invoke("execute2ValuesAndCarry", value, flag, flag));
     
     // Actualizar flags
     flagField.set(aluOp.field(FLAG));
