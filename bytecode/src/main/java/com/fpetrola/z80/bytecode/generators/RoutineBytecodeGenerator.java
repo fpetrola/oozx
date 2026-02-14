bc3d835ae7063c232fa0a6159aec3aabc8506b1e
Fernando Petrola <fernandop@sonatype.com>
2026-02-14 12:49:44 -0300

    feat: invert condition for direct context handling in RoutineBytecodeGenerator

--- bytecode/src/main/java/com/fpetrola/z80/bytecode/generators/RoutineBytecodeGenerator.java
+++ bytecode/src/main/java/com/fpetrola/z80/bytecode/generators/RoutineBytecodeGenerator.java
@@ -158,7 +158,7 @@ public class RoutineBytecodeGenerator {
             int rDelta = abstractInstruction.getRDelta();
 //            if (rDelta < 0)
 //              System.out.println("adgagadg");
-            if (context.direct)
+            if (!context.direct)
               mm.invoke("pc", context.pc.read().intValue(), rDelta);
           } else
             System.out.println("dsggag");
