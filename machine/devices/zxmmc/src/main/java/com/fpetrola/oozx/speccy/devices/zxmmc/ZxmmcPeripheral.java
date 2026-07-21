cc6936c3d9314ab2876ecf8c4671e57228cd2601
fernando <fpetrola@dc.uba.ar>
2026-07-20 21:19:42 -0300

    The ZXMMC: a card slot and nothing else
    The select at 0x1f and the card's byte at 0x3f. The smallest of them, and the
    one that shows how little a storage interface has to be.

---
+++ machine/devices/zxmmc/src/main/java/com/fpetrola/oozx/speccy/devices/zxmmc/ZxmmcPeripheral.java
@@ -0,0 +1,34 @@
+/*
+ *
+ *  * Copyright (c) 2023-2025 Fernando Damian Petrola
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+package com.fpetrola.oozx.speccy.devices.zxmmc;
+
+import com.fpetrola.oozx.speccy.devices.ide.MmcBoard;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * The ZXMMC: a card slot and nothing else, with the select at 0x1f and the card's byte at 0x3f.
+ */
+@Singleton
+public class ZxmmcPeripheral extends MmcBoard {
+
+  @Inject
+  public ZxmmcPeripheral() {
+    super(0x001f, 0x003f);
+  }
+}
